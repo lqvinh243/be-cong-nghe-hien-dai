84d672481ce975994a7d09a9c14c377e2d3d3cc9
Dylan <dylan@namtech.sg>
2021-09-11 21:30:07 +0700

    update api find category

--- src/core/usecases/category/queries/find-category/FindCategoryQueryOutput.ts
+++ src/core/usecases/category/queries/find-category/FindCategoryQueryOutput.ts
@@ -1,7 +1,7 @@
 import { Category } from '@domain/entities/category/Category';
 import { RefSchemaArray } from '@shared/decorators/RefSchema';
 import { PaginationResponse } from '@shared/usecase/PaginationResponse';
-import { IsArray, IsDateString, IsString, IsUUID } from 'class-validator';
+import { IsArray, IsDateString, IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class FindCategoryQueryData {
     @IsUUID()
@@ -13,10 +13,15 @@ export class FindCategoryQueryData {
     @IsString()
     name: string;
 
+    @IsUUID()
+    @IsOptional()
+    parentId: string | null;
+
     constructor(data: Category) {
         this.id = data.id;
         this.createdAt = data.createdAt;
         this.name = data.name;
+        this.parentId = data.parentId;
     }
 }
 
